00535cb8eaa943f0a712b13d5e2dd04195d2bc28
Tim Seymore <tim_seymore2012@hotmail.com>
2020-06-25 21:28:48 -0400

    Update todo_gui.py

--- src/todo_gui.py
+++ src/todo_gui.py
@@ -1,3 +1,19 @@
+from tkinter import *
+
+import tkinter as tk
 import todo
 
-# TODO: use tk to build a gui version
+top = tk.Tk()
+
+# widgets go here
+lb1 = Listbox(top)
+lb1.insert(1, "Todo Tracker")
+lb2 = Listbox(top)
+lb2.insert(1, "todo 1")
+
+
+lb1.pack()
+lb2.pack()
+
+top.mainloop()
+
