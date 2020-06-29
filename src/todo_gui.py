1202c19502b8a9e146b7aee27cb4d2bb3bac5a9d
Tim Seymore <tim_seymore2012@hotmail.com>
2020-06-28 20:55:52 -0400

    Update todo_gui.py

--- src/todo_gui.py
+++ src/todo_gui.py
@@ -10,20 +10,26 @@ class ToDoTrackerGui(ToDoTracker):
     def __init__(self):
         self.root = Task("ToDo Tracker")
         self.top = tk.Tk()
+        self.indent_space = "    "
 
     def main(self):
         # preprocessing details
         self.load_from_disk()
+        current_task = self.root
 
         # widgets go here
+
+        # show current task
+        # current_task.add_doable(Task("task 1"))
         lb1 = Listbox(self.top)
-        lb1.insert(1, "Todo Tracker")
-        lb2 = Listbox(self.top)
-        lb2.insert(1, "todo 1")
+        lb1.insert(1, current_task)
+        index = 2
+        for sub in current_task.nodes:
+            lb1.insert(index, self.indent_space + str(sub))
+            index += 1
 
         # pack widgets
         lb1.pack()
-        lb2.pack()
 
         # main loop
         self.top.mainloop()
