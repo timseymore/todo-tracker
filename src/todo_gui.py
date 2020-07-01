ece5dd4327d134d2775c4ca091f25f5892cf251b
Tim Seymore <tim_seymore2012@hotmail.com>
2020-07-01 19:22:33 -0400

    Update todo_gui.py

--- src/todo_gui.py
+++ src/todo_gui.py
@@ -19,15 +19,19 @@ class ToDoTrackerGui(ToDoTracker):
 
         # widgets go here #
 
+        # main frame
+        frame = Frame(self.top)
+        frame.pack()
+
         # show current task
-        task1 = Task("task 1")
-        task1.add_doable(ToDo("todo 1"))
-        task4 = Task("task 4")
-        task1.add_doable(task4)
-        task4.add_doable(ToDo("todo 2"))
-        current_task.add_doable(task1)  # test line only
-        current_task.add_doable(Task("task 2"))  # test line only
-        current_task.add_doable(Task("task 3"))  # test line only
+        # task1 = Task("task 1")
+        # task1.add_doable(ToDo("todo 1"))
+        # task4 = Task("task 4")
+        # task1.add_doable(task4)
+        # task4.add_doable(ToDo("todo 2"))
+        # current_task.add_doable(task1)  # test line only
+        # current_task.add_doable(Task("task 2"))  # test line only
+        # current_task.add_doable(Task("task 3"))  # test line only
         current_task.display_gui(self.indent_space, self.top)
 
         # main loop #
